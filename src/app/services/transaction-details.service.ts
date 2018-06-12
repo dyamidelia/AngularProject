c19b58ef6e3cb0726052827eb313610dfe880680
vputhan <vputhan@tibco.com>
2018-06-12 11:50:03 +0530

    Commit for adding styles to transaction page

--- src/app/services/transaction-details.service.ts
+++ src/app/services/transaction-details.service.ts
@@ -69,19 +69,19 @@ export class TransactionDetailsService {
         "status":"Completed",
         "timeStamp":"Jan-17-2018 | 02:18:37 PM",
         "state":"Received_from_PP",
-        "description":"Received a cash response message from SOAP protocol,Soap protocol ID is CAQH/2.2.0Back submitAckRetrieval Transaction" 
+        "description":"Received a cash response message from SOAP protocol,Soap protocol ID is CAQH/2.2.0Back submitAckRetrieval Transaction : Received_from_PP" 
       },
       {
         "status":"Completed",
         "timeStamp":"Jan-17-2018 | 02:18:37 PM",
         "state":"Request_from_PP",
-        "description":"Received a cash response message from SOAP protocol,Soap protocol ID is CAQH/2.2.0Back submitAckRetrieval Transaction" 
+        "description":"Received a cash response message from SOAP protocol,Soap protocol ID is CAQH/2.2.0Back submitAckRetrieval Transaction : Request_from_PP" 
       },
       {
         "status":"Inprogress",
         "timeStamp":"Jan-17-2018 | 02:18:37 PM",
         "state":"Validation",
-        "description":"Received a cash response message from SOAP protocol,Soap protocol ID is CAQH/2.2.0Back submitAckRetrieval Transaction" 
+        "description":"Received a cash response message from SOAP protocol,Soap protocol ID is CAQH/2.2.0Back submitAckRetrieval Transaction: Request_for_validation" 
       }
     ];
 
