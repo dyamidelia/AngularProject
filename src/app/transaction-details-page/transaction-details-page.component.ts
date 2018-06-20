0c3dcfcbed55ed3b4bbc878b193a9e0256cc4b51
agolajap <agolajap@tibco.com>
2018-06-20 17:26:57 +0530

    lint Errors cleared

--- src/app/transaction-details-page/transaction-details-page.component.ts
+++ src/app/transaction-details-page/transaction-details-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import {TransactionDetailsService} from '../services/transaction-details.service';
+import { TransactionDetailsService } from '../services/transaction-details.service';
 
 
 @Component({
@@ -10,34 +10,34 @@ import {TransactionDetailsService} from '../services/transaction-details.service
   providers: [TransactionDetailsService]
 })
 export class TransactionDetailsPageComponent implements OnInit {
-  transacationDetailsData:any;
-  transacationColumnsData:any;
-  transactionStatusData:any;
-  currentTransaction:any;
-  showFullStatus:boolean = false;
+  transacationDetailsData: any;
+  transacationColumnsData: any;
+  transactionStatusData: any;
+  currentTransaction: any;
+  showFullStatus = false;
   constructor(
     private route: ActivatedRoute,
-    private transactionDetailsService:TransactionDetailsService
+    private transactionDetailsService: TransactionDetailsService
   ) { }
 
   ngOnInit() {
-  	  	this.route.paramMap
-  		.subscribe(params => {
-         let transactionId=params.get('transactionId');
-         let userId = params.get('userId');
-         // if (transactionId && userId)
-         this.transacationDetailsData= this.transactionDetailsService.getTransactionDetails(transactionId,userId);
-         this.transacationColumnsData= this.transactionDetailsService.getDisplayNamesForColumns();
-         this.transactionStatusData = this.transactionDetailsService.getTransactionStatusResponse();
+    this.route.paramMap
+      .subscribe(params => {
+        const transactionId = params.get('transactionId');
+        const userId = params.get('userId');
+        // if (transactionId && userId)
+        this.transacationDetailsData = this.transactionDetailsService.getTransactionDetails(transactionId, userId);
+        this.transacationColumnsData = this.transactionDetailsService.getDisplayNamesForColumns();
+        this.transactionStatusData = this.transactionDetailsService.getTransactionStatusResponse();
 
-         this.transacationColumnsData = this.transacationColumnsData.map((item,index) => {
-            item['display_value']=this.transacationDetailsData[0][item.col_name];
-            return item;
-          })
-  		});
+        this.transacationColumnsData = this.transacationColumnsData.map((item, index) => {
+          item['display_value'] = this.transacationDetailsData[0][item.col_name];
+          return item;
+        });
+      });
   }
 
-  onTransactionStateChange(currentTransaction){
+  onTransactionStateChange(currentTransaction) {
     this.currentTransaction = currentTransaction;
   }
 }
