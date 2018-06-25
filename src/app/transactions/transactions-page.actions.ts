8a4904d98a74881ae3d4f9c791e9b21e5d5012e3
agolajap <agolajap@tibco.com>
2018-06-25 16:26:24 +0530

    cleared lint errors

--- src/app/transactions/transactions-page.actions.ts
+++ src/app/transactions/transactions-page.actions.ts
@@ -6,43 +6,43 @@ export const POST_TRANSACTIONS_SUCCESS = 'POST_TRANSACTIONS_SUCCESS';
 export const GET_COlUMNS_SUCCESS = 'GET_COlUMNS_SUCCESS';
 export const SHOW_HIDDEN_COLUMN = 'SHOW_HIDDEN_COLUMN';
 export const HIDE_COLUMN = 'HIDE_COLUMN';
-export const SAVE_COlUMNS_SUCCESS = "SAVE_COlUMNS_SUCCESS";
-
-export const startGetTransactions = (service:TransactionsService)=>{
-    return service.getTransactions()
-    .pipe(map(transactions => getTransactions(transactions)));
-}
-
-export const getTransactions = (transactions:any[])=>({
+export const SAVE_COlUMNS_SUCCESS = 'SAVE_COlUMNS_SUCCESS';
+export const getTransactions = (transactions: any[]) => ({
     type: GET_TRANSACTIONS_SUCCESS,
     transactions
 });
+export const startGetTransactions = (service: TransactionsService) => {
+    return service.getTransactions()
+        .pipe(map(transactions => getTransactions(transactions)));
+};
+
+
 
 export const addColumn = (column: any) => ({
     type: SHOW_HIDDEN_COLUMN,
     column
 });
 
-export const hideColumn = (column:any)=>({
+export const hideColumn = (column: any) => ({
     type: HIDE_COLUMN,
     column
 });
 
-export const startGetColumns = (service:TransactionsService)=>{
-    return service.getColumns()
-    .pipe(map(columns => getColumns(columns)));
-}
-
-export const getColumns = (columns:any[])=>({
+export const getColumns = (columns: any[]) => ({
     type: GET_COlUMNS_SUCCESS,
     columns
 });
 
-export const postColumns = ()=>({
+export const startGetColumns = (service: TransactionsService) => {
+    return service.getColumns()
+        .pipe(map(columns => getColumns(columns)));
+};
+
+export const postColumns = () => ({
     type: SAVE_COlUMNS_SUCCESS
 });
 
-export const startPostColumns = (service:TransactionsService, postData)=>{
+export const startPostColumns = (service: TransactionsService, postData) => {
     service.postColumns(postData);
 };
 
